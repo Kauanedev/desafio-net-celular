93fa618f3c28e7e205c9c0885b2a8e16534d4baa
Kauane Santos <kauanesantos.coding@gmail.com>
2024-09-30 12:25:46 -0300

    fix version

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,10 +1,7 @@
 namespace DesafioPOO.Models
 {
-    public class Iphone : Smartphone
+    public class Iphone(string numero, string modelo, string imei, int memoria) : Smartphone(numero, modelo, imei, memoria)
     {
-        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
-
-
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone");
