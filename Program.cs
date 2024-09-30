8fd8fe715e0e995d0e2c9ca5f7b08c7b9859aa15
Kauane Santos <kauanesantos.coding@gmail.com>
2024-09-30 12:26:20 -0300

    final commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia(numero: "912345678", modelo: "01", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "02 pro max", imei: "222222222", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
